915f5183911f9d4c705c6722a5d7731d79a81f9f
Christopher Borchert <cborchertrun@gmail.com>
2020-09-25 19:52:52 +0200

    update inflight panel

--- app/renderer/components/pages/InFlight/InFlight.tsx
+++ app/renderer/components/pages/InFlight/InFlight.tsx
@@ -5,7 +5,10 @@ import { useHistory } from 'react-router-dom';
 import ipcEventTypes from '../../../../shared/ipcEventTypes';
 import routes from '../../../constants/routes.json';
 
+import styles from './InFlight.module.css';
+
 type Response = {
+  id: any;
   body: any;
   req: any;
   res: any;
@@ -24,6 +27,8 @@ export default function InFlight(): JSX.Element {
   };
 
   const [responses, setResponses] = useState<Response[]>([]);
+
+  const [showResponse, setShowResponse] = useState<Response>();
 
   /**
    * On mount listen for new responses
@@ -53,13 +58,83 @@ export default function InFlight(): JSX.Element {
       <button type="button" onClick={goToLaunchPad}>
         Back
       </button>
-      <ul>
-        {responses.map(({ body, req = {}, res = {}, origin, id }) => (
-          <li key={id}>
-            {`${id}: ${req.url} ${req.method} [${res.statusCode}] (${origin})`}
-          </li>
-        ))}
-      </ul>
+
+      {showResponse ? (
+        <ResponseModal
+          response={showResponse}
+          onClose={() => setShowResponse(undefined)}
+        />
+      ) : (
+        <ResponseTable
+          responses={responses}
+          setShowResponse={setShowResponse}
+        />
+      )}
     </div>
   );
 }
+
+type ResponseTabeProps = {
+  responses: Response[];
+  setShowResponse: any;
+};
+/**
+ * Display the reponses
+ * @param props
+ */
+const ResponseTable = ({
+  responses = [],
+  setShowResponse,
+}: ResponseTabeProps): JSX.Element => (
+  <ul className={styles.responseTable}>
+    <li className={styles.responseHeader}>
+      <div>id</div>
+      <div>Method</div>
+      <div>Path</div>
+      <div>Status</div>
+      <div>Proxy/Mock</div>
+      <div>Response</div>
+      <div>...</div>
+    </li>
+    {responses.map((response) => {
+      const { req = {}, res = {}, origin, id } = response;
+      return (
+        <li className={styles.response} key={id}>
+          <div>{id}</div>
+          <div>{req.method}</div>
+          <div title={req.url}>{req.url}</div>
+          <div data-status={res.statusCode}>{res.statusCode}</div>
+          <div data-origin={origin}>{origin}</div>
+          <div>
+            <button onClick={() => setShowResponse(response)} type="button">
+              <i className="far fa-eye" />
+            </button>
+          </div>
+          <div>...</div>
+        </li>
+      );
+    })}
+  </ul>
+);
+
+type ResponseModalProps = {
+  response: Response;
+  onClose: any;
+};
+
+const ResponseModal = ({
+  response,
+  onClose,
+}: ResponseModalProps): JSX.Element => (
+  <div className={styles.modal}>
+    <div className={styles.modalHeader}>
+      <span>{`[${response.req.method}] ${response.req.url}`}</span>
+      <button onClick={onClose} type="button">
+        close
+      </button>
+    </div>
+    <pre className={styles.modalInner}>
+      {JSON.stringify(response.body || {}, null, '  ')}
+    </pre>
+  </div>
+);
